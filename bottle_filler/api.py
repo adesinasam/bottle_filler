1e2e5af3e0719ab72dfb84e92c31c11c9954ca83
Adesina Akinyemi <59088328+adesinasam@users.noreply.github.com>
2024-07-23 08:30:53 +0100

    Update api.py

--- bottle_filler/api.py
+++ bottle_filler/api.py
@@ -2,29 +2,44 @@ import frappe
 from frappe import _
 from frappe import utils
 
+
 def setup(sales_invoice, method):
+
     invoice_items = []
     
     for detail in sales_invoice.items:
         if hasattr(detail, 'has_empty_bottle') and not hasattr(detail, 'allow_is_pos'):
             invoice_items.append(detail)
 
-    sales_invoice.items = invoice_items
-    make_stock_entry(sales_invoice)
+            sales_invoice.items = invoice_items
+            make_stock_entry(sales_invoice)
+
+        if hasattr(detail, 'has_empty_bottle') and hasattr(detail, 'allow_is_pos'):
+            invoice_items.append(detail)
+
+            sales_invoice.items = invoice_items
+            make_pos_empty_entry(sales_invoice)
+
 
 def make_stock_entry(sales_invoice):
-    if sales_invoice.docstatus == 1:
-        # Check for duplicates
+
+    if sales_invoice.docstatus == 1:         
+
+        # check for duplicates
         if frappe.db.exists({'doctype': 'Stock Entry', 'salesinvoiceno': sales_invoice.name}):
             frappe.throw(
                 title="Error",
                 msg="Stock Entry for Invoice {} already exists.".format(sales_invoice.name)
             )
 
+
         # Preparing the SE: convert sales_invoice details into se items details
+
         items = []
-        for detail in sales_invoice.items:
-            items.append({
+
+        for detail in sales_invoice.items:            
+
+            items.append({  
                 't_warehouse': detail.warehouse,
                 'item_code': detail.empty_bottle_item_code,
                 'qty': detail.invoice_account,
@@ -33,7 +48,8 @@ def make_stock_entry(sales_invoice):
                 'cost_center': detail.cost_center
             })
 
-        # Create the stock entry
+
+        # create the stock entry
         se = frappe.get_doc({
             'title': "Auto Material Receipt",
             'doctype': 'Stock Entry',
@@ -49,11 +65,12 @@ def make_stock_entry(sales_invoice):
             'salesinvoiceno': sales_invoice.name
         })
 
+
         se.insert()
         se.submit()
 
-        # Create the empty bottle entry
-        for detail in sales_invoice.items:
+        # create the stock entry
+        for detail in sales_invoice.items:            
             ebe = frappe.get_doc({
                 'doctype': 'Empty Bottle Entry',
                 'item_code': detail.item_code,
@@ -78,16 +95,21 @@ def make_stock_entry(sales_invoice):
                 'territory': sales_invoice.territory
             })
 
+
             ebe.insert()
     
     elif sales_invoice.docstatus == 2:
-        pr_name = frappe.db.get_value("Stock Entry", {"salesinvoiceno": sales_invoice.name}, "name")
+
+        pr_name = frappe.db.get_value("Stock Entry",{"salesinvoiceno": sales_invoice.name}, "name")
+        
         pr = frappe.get_doc("Stock Entry", pr_name)
         pr.cancel()
 
-        for detail in sales_invoice.items:
+        for detail in sales_invoice.items:            
             ebe = frappe.get_doc({
                 'doctype': 'Empty Bottle Entry',
                 'is_cancelled': 1
             })
+
+
             ebe.save()
