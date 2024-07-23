a1223f39a7dc7520eae96fffff3e6434fa3a3bbf
Adesina Akinyemi <59088328+adesinasam@users.noreply.github.com>
2024-07-23 08:51:33 +0100

    Update api.py

--- bottle_filler/api.py
+++ bottle_filler/api.py
@@ -58,11 +58,11 @@ def make_stock_entry(sales_invoice):
             'title': "Auto Material Receipt",
             'doctype': 'Stock Entry',
             'stock_entry_type': 'Material Receipt',
+            'purpose': 'Material Receipt',
             'posting_date': sales_invoice.posting_date,
             'posting_time': sales_invoice.posting_time,
             'set_posting_time': 1,
             'company': sales_invoice.company,
-            'to_warehouse': sales_invoice.set_warehouse,
             'items': items,
             'remarks': 'Being Empty Entry',
             'project': sales_invoice.project,
@@ -74,33 +74,33 @@ def make_stock_entry(sales_invoice):
         se.submit()
 
         # create the stock entry
-        for detail in sales_invoice.items:            
-            ebe = frappe.get_doc({
-                'doctype': 'Empty Bottle Entry',
-                'item_code': detail.item_code,
-                'item_name': detail.item_name,
-                'warehouse': detail.warehouse,
-                'posting_date': sales_invoice.posting_date,
-                'posting_time': sales_invoice.posting_time,
-                'empty_item_code': detail.empty_item_code,
-                'empty_item_name': detail.empty_item_name,
-                'voucher_type': 'Sales Invoice',
-                'voucher_no': sales_invoice.name,
-                'actual_qty': detail.qty,
-                'price': float(detail.rate),
-                'amount': float(detail.amount),
-                'customer': sales_invoice.set_warehouse,
-                'empty_qty': detail.empty_bottle_qty,
-                'empty_price': float(detail.empty_bottle_rate),
-                'empty_amount': float(detail.empty_bottle_amount),
-                'difference_in_qty': detail.qty - detail.empty_bottle_qty,
-                'company': sales_invoice.company,
-                'cost_center': detail.cost_center,
-                'territory': sales_invoice.territory
-            })
+        # for detail in sales_invoice.items:            
+        #     be = frappe.get_doc({
+        #         'doctype': 'Empty Bottle Entry',
+        #         'item_code': detail.item_code,
+        #         'item_name': detail.item_name,
+        #         'warehouse': detail.warehouse,
+        #         'posting_date': sales_invoice.posting_date,
+        #         'posting_time': sales_invoice.posting_time,
+        #         'empty_item_code': detail.empty_item_code,
+        #         'empty_item_name': detail.empty_item_name,
+        #         'voucher_type': 'Sales Invoice',
+        #         'voucher_no': sales_invoice.name,
+        #         'actual_qty': detail.qty,
+        #         'price': float(detail.rate),
+        #         'amount': float(detail.amount),
+        #         'customer': sales_invoice.set_warehouse,
+        #         'empty_qty': detail.empty_bottle_qty,
+        #         'empty_price': float(detail.empty_bottle_rate),
+        #         'empty_amount': float(detail.empty_bottle_amount),
+        #         'difference_in_qty': detail.qty - detail.empty_bottle_qty,
+        #         'company': sales_invoice.company,
+        #         'cost_center': detail.cost_center,
+        #         'territory': sales_invoice.territory
+        #     })
 
 
-            ebe.insert()
+        #     be.insert()
     
     # elif sales_invoice.docstatus == 2:
 
