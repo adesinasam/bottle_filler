72c0d16073be76643d3688c1eac17e25769ea1ac
Adesina Akinyemi <59088328+adesinasam@users.noreply.github.com>
2024-07-24 12:54:08 +0100

    Update sales_invoice.js

--- bottle_filler/public/js/doctype_plugin/sales_invoice.js
+++ bottle_filler/public/js/doctype_plugin/sales_invoice.js
@@ -1,7 +1,7 @@
 frappe.ui.form.on('Sales Invoice', {
     validate: function(frm) {
         $.each(frm.doc.items || [], function(i, item) {
-            if (item.has_empty_bottle && !item.allow_in_pos && item.empty_bottle_qty===0) {
+            if (item.has_empty_bottle && !item.allow_in_pos && !frm.doc.is_pos && item.empty_bottle_qty===0) {
                 frappe.msgprint(__('Row #{0}: Empty Bottle Quantity cannot be zero.', [item.idx]));
                 frappe.validated = false;
                 return false;
